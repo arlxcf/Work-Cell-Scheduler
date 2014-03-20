f03a4ffc2dc0bd0259d6edddbb5daacf34322d64
Andrew Lazalier <arlxcf@mail.missouri.edu>
2014-03-20 12:54:53 -0500

    Change of DB data

--- Config/database.sql
+++ Config/database.sql
@@ -1,12 +1,11 @@
 --  Database Copyright 2014 by WebIS Spring 2014 License Apache 2.0
 
 -- Person
-DROP TABLE IF EXISTS Person;
-CREATE TABLE Person (
-  person VARCHAR(30),
+DROP TABLE IF EXISTS worker;
+CREATE TABLE Worker (
+  worker VARCHAR(30),
   name VARCHAR(128),
-  rate FLOAT,
-  PRIMARY KEY (person)
+  PRIMARY KEY (worker)
 );
 
 -- SELECT * FROM Person;
@@ -14,19 +13,17 @@ CREATE TABLE Person (
 -- Training Matrix
 DROP TABLE IF EXISTS TrainingMatrix;
 CREATE TABLE TrainingMatrix (
-  person VARCHAR(30),
-  cell integer,
-  workstation integer,
-  wcp double,
-  wsp double,
-  PRIMARY KEY (person,cell,workstation)
+  worker VARCHAR(30),
+  subcell integer,
+  training double,
+  PRIMARY KEY (worker,subcell,training)
 );
 
-INSERT INTO TrainingMatrix (person,cell,workstation,wcp,wsp) VALUES 
-	('Dr.Middelkoop',1000,1010,0.19,0.19),
-	('Dr.Middelkoop',1000,1020,0.55,0.32),
-	('JD',1000,1010,0.99,0.99),
-	('JD',1000,1030,0.90,0.10);
+INSERT INTO TrainingMatrix (worker,subcell,training) VALUES 
+	('Dr.Middelkoop',1000,0.19),
+	('Dr.Middelkoop',1010,0.32),
+	('JD',1020,0.98),
+	('JD',1030,0.12);
 
 	
 -- SELECT * FROM TrainingMatrix;
